5f3dab5d1e02736fc88778e118fb5db2813fcac5
Mathan Eelam <mathaneelam@gmail.com>
2020-08-02 18:26:55 +0530

    creat HOC and Aux method

--- react-complete-guide/src/Components/Persons/Person/Person.js
+++ react-complete-guide/src/Components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 
 
@@ -7,12 +8,15 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            // <div className="Person" style={style}>
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and i am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+                <p key="i2">{this.props.children}</p>
+                <input key="i3"
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name} 
+                />
+            </Aux>   
         );
     }
 };

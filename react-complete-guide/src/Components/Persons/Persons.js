eb02e5de6ebe25ad4a3f2a9c35c1d054cbde2210
Mathan Eelam <mathaneelam@gmail.com>
2020-08-04 07:25:10 +0530

    add auth context and more...

--- react-complete-guide/src/Components/Persons/Persons.js
+++ react-complete-guide/src/Components/Persons/Persons.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
+
 class Persons extends PureComponent {
     // static getDerivedStateFromProps(props, state) {
     //     console.log('[Persons.js] getDerivedStateFromProps');
@@ -47,13 +48,16 @@ class Persons extends PureComponent {
         console.log('[Persons.js] rendering....'); 
 
         return this.props.persons.map((person, index) => {
-            return <Person 
+            return (
+            <Person 
                 click={() => this.props.clicked(index)}
                 name={person.name} 
                 age={person.age}
                 key={person.id} 
-                changed={(event) => this.props.changed(event, person.id)}/>
-            });
+                changed={(event) => this.props.changed(event, person.id)}
+                />
+            );    
+        });
     }
 } 
 
--- react-complete-guide/src/Containers/App.js
+++ react-complete-guide/src/Containers/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../Components/Persons/Persons';
 import Cockpit from '../Components/Cockpit/Cockpit';
-
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxiliary';
+import AuthContext from '../context/auth.context';
 
 class App extends Component {
   constructor(props) {
@@ -12,13 +14,15 @@ class App extends Component {
 
   state = {
     persons: [
-      {id:'PerKey1', name: 'Max', age: '28'},
-      {id:'Perkey2', name: 'Manu', age: '27'},
-      {id:'PerKey3', name: 'Nachiyar', age: '26'}
+      {id:'PerKey1', name: 'Max', age: 28},
+      {id:'Perkey2', name: 'Manu', age: 27},
+      {id:'PerKey3', name: 'Nachiyar', age: 26}
     ],
     otherState: 'Some other value',
     showPersons: false,
-    showCockpit : true,
+    showCockpit: true,
+    changeCounter: 0,
+    authenticated: false,
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -60,8 +64,13 @@ class App extends Component {
     const persons =[...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons})
-  }
+    this.setState((prevState, props) => {
+      return {
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1,
+      };
+    });
+  };
 
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice;
@@ -75,6 +84,10 @@ class App extends Component {
     this.setState({showPersons: !doesShow});
   }
 
+  loginHandler = () => {
+    this.setState({authenticated: true});
+  };
+
   render() {
     console.log('[App.js render]')
 
@@ -83,28 +96,38 @@ class App extends Component {
       persons = <Persons 
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
-          changed={this.nameChangedHandler}/>;
+          changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
+          />;
     }
 
     return (
-      <div className={classes.App}>
+      <Aux>
         <button 
         onClick={() =>{
           this.setState({ showCockpit: false })
         }}>
         Remove Cockpit
         </button>
+        <AuthContext.Provider 
+          value={{
+            authenticated: this.state.authenticated,
+            login: this.loginHandler,
+          }}
+        >
         {this.state.showCockpit ? (<Cockpit 
         title={this.props.appTitle}
         showPersons={this.state.showPersons}
         personsLength={this.state.persons.length}
-        clicked={this.togglePersonsHandler}/>
+        clicked={this.togglePersonsHandler}
+        />
         ) : null}
-        {persons}    
-      </div>
+        {persons}
+        </AuthContext.Provider>   
+      </Aux>
     );
     // return React.createElement('div', {className: 'App'}, null, React.createElement('h1', null, ' It works now'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);

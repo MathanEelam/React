34a41ec16daa43291be0d6b31ed36234406679e9
Mathan Eelam <mathaneelam@gmail.com>
2020-08-14 08:45:04 +0530

    add backdrop and sidedrawer

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component{
+    state = {
+        ShowSideDrawer: true,
+    }
 
-export default layout;
+    SideDrawerClosedHandler = () => {
+        this.setState({ShowSideDrawer: false});
+    }
+    render () {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.ShowSideDrawer} closed={this.SideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    };
+};
+
+export default Layout;

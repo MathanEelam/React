916c4d7fa697f49194bab44d22c9637c11aae737
Mathan Eelam <mathaneelam@gmail.com>
2020-08-17 07:07:25 +0530

    setup firebase and store data to firebase

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+
 
 const INGREDIENT_PRICE = {
     salad: 0.5,
@@ -81,7 +83,27 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue');
+        // alert('You continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Mathan Eelam',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '641641',
+                    country: 'India'
+                },
+                email: 'mathan@gmail.com',
+            },
+            deliveryMethod: 'fastest',
+        }
+        axios.post('/orders.json', order)
+        .then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        });
     }
 
     render () {

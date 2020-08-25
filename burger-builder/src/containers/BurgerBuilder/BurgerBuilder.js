a84c9c7394046814b15ace18ea241755b63d025c
Mathan Eelam <mathaneelam@gmail.com>
2020-08-25 06:49:17 +0530

    add routing and ckeckout, continued handler

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,6 +32,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('https://burger-builder-0404.firebaseio.com/ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data});
@@ -92,28 +93,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Mathan Eelam',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '641641',
-                    country: 'India'
-                },
-                email: 'mathan@gmail.com',
-            },
-            deliveryMethod: 'fastest',
-        }
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing: false});
-        }).catch(error => {
-            this.setState({loading: false, purchasing: false});
-        });
+    //     // alert('You continue');
+    //     this.setState({loading: true});
+    //     const order = {
+    //         ingredients: this.state.ingredients,
+    //         price: this.state.totalPrice,
+    //         customer: {
+    //             name: 'Mathan Eelam',
+    //             address: {
+    //                 street: 'Teststreet 1',
+    //                 zipCode: '641641',
+    //                 country: 'India'
+    //             },
+    //             email: 'mathan@gmail.com',
+    //         },
+    //         deliveryMethod: 'fastest',
+    //     }
+    //     axios.post('/orders.json', order)
+    //     .then(response => {
+    //         this.setState({loading: false, purchasing: false});
+    //     }).catch(error => {
+    //         this.setState({loading: false, purchasing: false});
+    //     });
+        this.props.history.push('/checkout');
     }
 
     render () {
---
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+class Checkout extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1,
+        }
+    }
+
+    CheckoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    CheckoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
+
+    render() {
+        return (
+            <div>
+                <CheckoutSummary 
+                    ingredients={this.state.ingredients}
+                    onCheckoutCancelled={this.CheckoutCancelledHandler}
+                    onCheckoutContinued={this.CheckoutContinuedHandler}
+                />
+            </div>
+        );
+    }
+}
+
+export default Checkout;
